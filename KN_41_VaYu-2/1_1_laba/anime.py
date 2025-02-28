c0f61cbfc167b99599ea943699aebf7bf9d1b855
VasylynaYurij <yuvasylyna2021@itcollege.lviv.ua>
2025-02-28 14:41:51 +0200

    w

--- KN_41_VaYu-2/1_1_laba/anime.py
+++ KN_41_VaYu-2/1_1_laba/anime.py
@@ -4,15 +4,17 @@ from jikanpy import Jikan
 jikan = Jikan()
 app = Flask(__name__)
 
-j = jikan.anime(54595, extension='episodes')
-
 @app.route('/')
 def home():
+    # Відкриваємо anime ID 54595
+    anime_data = jikan.anime(54595)  # Тепер без параметра extension='episodes'
+    
+    # Показуємо інформацію про епізоди
     a = str()
-    for episode in j["data"]: 
+    for episode in anime_data["episodes"]:  # Взяти список епізодів
         a += f"<p>Епізод {episode['mal_id']} з назвою: {episode['title']} має оцінку {episode['score']}<p>"
+    
     return a
 
 if __name__ == '__main__':
     app.run(debug=True)
-
