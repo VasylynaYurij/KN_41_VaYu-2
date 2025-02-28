aea35b227a0a0329cba1d93eb5778b9523dfd853
VasylynaYurij <yuvasylyna2021@itcollege.lviv.ua>
2025-02-28 14:22:23 +0200

    В

---
+++ KN_41_VaYu-2/1_1_laba/anime.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+from jikanpy import Jikan
+
+jikan = Jikan()
+app = Flask(__name__)
+
+j = jikan.anime(54595, extension='episodes')
+
+@app.route('/')
+def home():
+    a = str()
+    for episode in j["data"]: 
+        a += f"<p>Епізод {episode['mal_id']} з назвою: {episode['title']} має оцінку {episode['score']}<p>"
+    return a
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
