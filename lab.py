d177f214b741b907374ec1fb31448730a4a8568a
VasylynaYurij <yuvasylyna2021@itcollege.lviv.ua>
2025-04-10 19:03:07 +0300

    sdsd

--- lab.py
+++ lab.py
@@ -1,7 +1,17 @@
-# Перевірка assert
-number = -1
-try:
-    assert number > 0, "Число має бути більшим за нуль!"
-except AssertionError as e:
-    print(f"Помилка: {e}")
+def test_get_angles():
+    """Тестуємо чи правильно повертається кількість кутів фігури."""
+    fig = "трикутник"
+    triangle = Figure(fig, 1)
+    assert triangle.get_angles == 3, f"У {fig} є 3 кути!"
 
+class Figure:
+    def __init__(self, type, sides):
+        self.type = type
+        self.sides = sides
+
+    @property
+    def get_angles(self):
+        if self.type in ["квадрат", "прямокутник"]:
+            return 4
+        if self.type == "трикутник":
+            return 3
